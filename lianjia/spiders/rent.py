ebdf31e4dd2cca704563331d91a4b2cc456d4857
peritot <peritotchan@gmail.com>
2018-05-17 17:03:23 +0800

    Spider with district

--- lianjia/spiders/rent.py
+++ lianjia/spiders/rent.py
@@ -14,13 +14,12 @@ llPattern = re.compile('<script>.*?resblockPosition:\'(.*?)\',.*?</script>', re.
 
 PROXY_ENABLE = properties.PROXY_ENABLE
 PROXY_POOL_URL = properties.PROXY_POOL_URL
-MAX_PAGE = properties.MAX_PAGE
-proxy = ''
 
 
 class RentSpider(scrapy.Spider):
     name = 'rent'
     allowed_domains = ['lianjia.com']
+    start_urls = ['https://wh.lianjia.com/zufang']
 
     def get_proxy(self):
         if PROXY_ENABLE != True:
@@ -34,41 +33,65 @@ class RentSpider(scrapy.Spider):
         except ConnectionError:
             return ''
 
-    def start_requests(self):
-        for i in range(1, MAX_PAGE + 1):
-            if i == MAX_PAGE:
-                i = 1
-
-            url = 'https://wh.lianjia.com/zufang/pg{pg}'.format(pg=i)
-            proxy = self.get_proxy()
-            time.sleep(5)
-
-            yield scrapy.Request(url, callback=self.parse, meta={'proxy': proxy})
-
     def parse(self, response):
+        for href in response.css('#filter-options dd[data-index="0"] .option-list a[class!="on"]::attr(href)').extract():
+            try:
+                href = 'https://wh.lianjia.com' + href
+                proxy = self.get_proxy()
+                yield scrapy.Request(href, callback=self.parse_district, meta={'proxy': proxy})
+            except Exception as error:
+                print(error)
+
+    def parse_district(self, response):
+        for href in response.css('#filter-options dd[data-index="0"] .sub-option-list a[class!="on"]::attr(href)').extract():
+            try:
+                href = 'https://wh.lianjia.com' + href
+                proxy = response.meta['proxy']
+                yield scrapy.Request(href, callback=self.parse_street, meta={'proxy': proxy})
+            except Exception as error:
+                print(error)
+
+    def parse_street(self, response):
+        try:
+            url = 'https://wh.lianjia.com' + response.css('.house-lst-page-box::attr(page-url)').extract()[0].strip()
+            pgdata = response.css('.house-lst-page-box::attr(page-data)').extract()[0].strip()
+            pgdict = eval(pgdata)
+            total = pgdict['totalPage']
+            for pg in (1, total):
+                href = url.replace('/{page/}', str(pg))
+                proxy = self.get_proxy()
+                yield scrapy.Request(href, callback=self.parse_pg, meta={'proxy': proxy})
+        except Exception as error:
+            print(error)
+
+    def parse_pg(self, response):
         for li in response.css('#house-lst li'):
-            item = RentItem()
-            item['title'] = li.css('.info-panel h2 a::text').extract()[0].strip()
-            item['community'] = li.css('.region::text').extract()[0].strip()
-            item['zone'] = li.css('.zone span::text').extract()[0].strip()
-            item['price'] = li.css('.price .num::text').extract()[0].strip()
+            try:
+                item = RentItem()
+                item['title'] = li.css('.info-panel h2 a::text').extract()[0].strip()
+                item['community'] = li.css('.region::text').extract()[0].strip()
+                item['zone'] = li.css('.zone span::text').extract()[0].strip()
+                item['price'] = li.css('.price .num::text').extract()[0].strip()
 
-            # area
-            area = li.css('.meters::text').extract()[0].strip()
-            area = re.findall(numPattern, area)[0]
-            item['area'] = area
+                # area
+                area = li.css('.meters::text').extract()[0].strip()
+                area = re.findall(numPattern, area)[0]
+                item['area'] = area
 
-            # time
-            time = li.css('.price-pre::text').extract()[0].strip()
-            time = time.replace(' 更新', '').replace('.', '-')
-            item['time'] = time
+                # time
+                time = li.css('.price-pre::text').extract()[0].strip()
+                time = time.replace(' 更新', '').replace('.', '-')
+                item['time'] = time
 
-            # link
-            link = li.css('.info-panel h2 a::attr(href)').extract()[0].strip()
-            item['link'] = link
-            item['id'] = re.findall(numPattern, link)[0]
+                # link
+                link = li.css('.info-panel h2 a::attr(href)').extract()[0].strip()
+                item['link'] = link
+                item['id'] = re.findall(numPattern, link)[0]
 
-            yield scrapy.Request(link, callback=self.parse_detail, meta={'proxy': proxy, 'item': item})
+                proxy = response.meta['proxy']
+                yield scrapy.Request(link, callback=self.parse_detail, meta={'proxy': proxy, 'item': item})
+            except Exception as error:
+                print(error)
 
     def parse_detail(self, response):
         item = response.meta['item']
@@ -82,7 +105,6 @@ class RentSpider(scrapy.Spider):
             ll = re.findall(llPattern, res)[0].split(',')
             item['longitude'] = ll[0]
             item['latitude'] = ll[1]
-            print(item)
 
             return item
         except Exception:
